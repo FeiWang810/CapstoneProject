ab1d6e23b4f9bd2f640ef3e18455437ed85a0cfe
王菲 <wangfei@ousMacBookpuro.fios-router.home>
2018-12-03 12:19:26 -0500

    third version

---
+++ Fei_Coding/reviewExploration/dataGenerator/reviewGenerator.py
@@ -0,0 +1,75 @@
+import keras
+from keras import layers
+import sys
+import numpy as np
+from keras.utils import plot_model
+from IPython.display import Image
+
+
+# this text file can be any text, as long as it contains text longer than 'maxlen' defined below
+text=open('seed_text.txt').read()
+chars=['\n', ' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', '>', '?', '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', '\\', ']', '^', '_', '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~']
+
+# Dictionary mapping unique characters to their index in `chars`
+char_indices = dict((char, chars.index(char)) for char in chars)
+maxlen=60
+step=1
+
+model = keras.models.Sequential()
+model.add(layers.LSTM(1024, input_shape=(maxlen, len(chars)),return_sequences=True))
+model.add(layers.LSTM(1024, input_shape=(maxlen, len(chars))))
+model.add(layers.Dense(len(chars), activation='softmax'))
+model.load_weights("pre-trained.hdf5")
+
+optimizer = keras.optimizers.Adam(lr=0.0002)
+model.compile(loss='categorical_crossentropy', optimizer=optimizer)
+
+plot_model(model, to_file='model.png', show_shapes=True)
+Image(filename='model.png')
+
+def sample(preds, temperature=1.0):
+    '''
+    Generate some randomness with the given preds
+    which is a list of numbers, if the temperature
+    is very small, it will always pick the index
+    with highest pred value
+    '''
+    preds = np.asarray(preds).astype('float64')
+    preds = np.log(preds) / temperature
+    exp_preds = np.exp(preds)
+    preds = exp_preds / np.sum(exp_preds)
+    probas = np.random.multinomial(1, preds, 1)
+    return np.argmax(probas)
+
+def random_reviews():
+    # sample a start index
+    start_index = np.random.randint(0, len(text) - maxlen - 1)
+    # the initial sampled text with maxlen long
+    generated_text = text[start_index: start_index + maxlen]
+    print('Coming up with several reviews for you...')
+
+    for temperature in [0.8]:
+        sys.stdout.write(generated_text)
+
+        # We generate 600 characters
+        for i in range(600):
+            sampled = np.zeros((1, maxlen, len(chars)))
+            # Turn each char to char index.
+            for t, char in enumerate(generated_text):
+                sampled[0, t, char_indices[char]] = 1.
+            # Predict next char probabilities
+            preds = model.predict(sampled, verbose=0)[0]
+            # Add some randomness by sampling given probabilities.
+            next_index = sample(preds, temperature)
+            # Turn char index to char.
+            next_char = chars[next_index]
+            # Append char to generated text string
+            generated_text += next_char
+            # Pop the first char in generated text string.
+            generated_text = generated_text[1:]
+            # Print the new generated char.
+            sys.stdout.write(next_char)
+            sys.stdout.flush()
+
+if __name__ == "__main__":
+    random_reviews()
